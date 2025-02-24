5eba2a87720d6be3ee7ea215a23ed97959fa89a3
arun <arunps0gmail.com>
2025-02-24 10:26:38 +0530

    bcrypt update

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,14 +2,20 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
-  @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  @Post(`userReg`)
+  userReg(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.userReg(createUserDto);
+  }
+
+  @Post(`userLogin`)
+  userLogin(@Body() userLoginDto: LoginUserDto){
+    return this.usersService.userLogin(userLoginDto);
   }
 
   @Get()
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,33 +2,55 @@ import {
   ConflictException,
   Injectable,
   NotFoundException,
+  UnauthorizedException,
 } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(
     @InjectRepository(User) private readonly usersRepository: Repository<User>,
   ) {}
-  public async create(createUserDto: CreateUserDto) {
+  public async userReg(createUserDto: CreateUserDto) {
+    const saltRounds: number = 10;
     const { username, email, password } = createUserDto;
-    const extUser = await this.usersRepository.findOneBy({ email });
+    const extUser = await this.usersRepository.findOne({
+      where: [{ username }, { email }],
+    });
     if (extUser) {
       throw new ConflictException('User already exists');
     } else {
+      const hashedPassword = await bcrypt.hash(password, saltRounds);
       const newUser = this.usersRepository.create({
         username,
         email,
-        password,
+        password: hashedPassword,
       });
       await this.usersRepository.save(newUser);
       return newUser;
     }
   }
+
+
+  public async userLogin(userLoginDto: LoginUserDto) {
+    const {email, password} = userLoginDto;
+    const user = await this.usersRepository.findOne({where: { email }});
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      throw new UnauthorizedException('Invalid credentials');
+    }
+    return `Successfully logged in`;
+  }
+
 
   findAll() {
     return this.usersRepository.find();
